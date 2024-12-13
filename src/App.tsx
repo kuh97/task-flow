7e4fc3d6e665707dda15987001d66db0357c82fd
kimuihyeon <rladml91gus@naver.com>
2024-12-13 14:44:40 +0900

    [feat] add Zustand state management library and example implementation
    - Installed Zustand library for state management
    - Created a simple counter store as an example
    - Demonstrated usage of Zustand in a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Counter from "./components/Counter";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  // return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+// 예시 코드
+export interface TotalCount {
+  totalCount: number;
+  setTotalCount: (count: number) => void;
+}
